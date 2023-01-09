e74f360fbb4cad8cee9f931e8c99d66f154de215
Corey <corey.powe@gmail.com>
2023-01-09 16:22:55 -0500

    Updated Banner text

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -20,7 +20,7 @@ function Footer() {
       <p className="m-0">
         Copyright &copy; 2020{" "}
         <Link to="/" className="text-muted">
-          ComplexApp for React
+          ComplexApp for React development
         </Link>
         . All rights reserved.
       </p>
--- app/components/Header.js
+++ app/components/Header.js
@@ -14,7 +14,7 @@ function Header(props) {
         <h4 className="my-0 mr-md-auto font-weight-normal">
           <Link to="/" className="text-white">
             {" "}
-            ComplexApp for React{" "}
+            ComplexApp for React development{" "}
           </Link>
         </h4>
         {!props.statcEmpty ? headerContent : ""}
